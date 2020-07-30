e19f6d1d111480c1f66a218ded91d0e1e3dca7cf
liun zheng <raphaelwwj@gmail.com>
2020-07-29 23:38:26 -0400

    update ex1-5

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+print ("Hello world!")
+print ("Hello Again")
+print ("I like typing this.")
+print ("This is fun")
+print ('Yay! Printing.')
+print ("I'd mush rather your 'not'.")
+print ('i "siad" do not touch this.')
